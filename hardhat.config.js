333724a71bace8c41e4059fb8093d82f719eea65
alfred <lianghongbuaa@gmail.com>
2023-11-10 10:45:23 +0800

    add testnet deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv/config");
 require('@nomiclabs/hardhat-ethers');
+
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+    },
+  }
 };
