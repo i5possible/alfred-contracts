559692ce72cf725dfae6cd2660eeb1aefc8e4b46
alfred <lianghongbuaa@gmail.com>
2023-11-20 17:50:59 +0800

    add transfer test

--- test/HappyToken.test.js
+++ test/HappyToken.test.js
@@ -18,4 +18,17 @@ describe('HappyToken', function () {
         const balance = await this.happyToken.balanceOf(account);
         expect(balance.toString()).to.equal(mintAmout);
     });
+
+    it('should be able to transfer tokens', async function () {
+        const signerAddress = (await ethers.getSigners())[0].address;
+        console.log(signerAddress);
+        const mintAmout = '1000';
+        await this.happyToken.mint(signerAddress, mintAmout);
+        await this.happyToken.balanceOf(signerAddress);
+        await this.happyToken.transfer('0x0529Ff64e5B025a2ea31148e9A5b7fDe5AAfAfd0', 500);
+        const balanceAfter = await this.happyToken.balanceOf(signerAddress);
+        expect(balanceAfter.toString()).to.equal('500');
+        const balanceAfterReceiver = await this.happyToken.balanceOf('0x0529Ff64e5B025a2ea31148e9A5b7fDe5AAfAfd0');
+        expect(balanceAfterReceiver.toString()).to.equal('500');
+    });
 });
